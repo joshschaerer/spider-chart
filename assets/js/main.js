ae1913e8fbc408fe6a5a8c29f990ab114efd385d
joshschaerer <joshua.schaerer@students.fhnw.ch>
2023-12-04 00:03:00 +0100

    implemented new features
    - [1] divided features in different charts
    - [2] added buttons to download the svg images of each chart
    - [3] added small circles where the polygon intersects with the axes of the graph
    - [4] rotated all labels to be perpendicular to the center

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@
 
 /*---------- SETUP ----------*/
 // Initialize all data
-let features = [
+let features1 = [
   "Deutsch",
   "Englisch",
   "Französisch",
@@ -17,49 +17,64 @@ let features = [
   "Bildnerisches, Textiles und Technisches Gestalten",
   "Musik",
   "Bewegung und Sport",
+];
+let features2 = [
   "Pünktlichkeit",
   "Aktive Beteiligung",
   "Selbständigkeit",
   "Zuverlässigkeit",
   "Selbsteinschätzung",
   "Rückmeldungen nutzen",
+];
+let features3 = [
   "Angemessene Umgangsformen",
   "Hilfsbereitschaft und Respekt",
   "Zusammenarbeit",
   "Regeln einhalten",
   "Kommunikation",
 ];
-let selectedFeatures = [
-  "Deutsch",
-  "Englisch",
-  "Französisch",
-  "Mathematik",
-  "Natur, Mensch, Gesellschaft",
-];
-let selectedValues = [10, 10, 10, 10, 10];
 
 // Add all data to all dropdowns
-var dropdowns = document.querySelectorAll(".dd-features");
+var dropdowns1 = document
+  .getElementById("section-1")
+  .querySelectorAll(".dd-features");
+fillDropdown(dropdowns1, features1);
 
-for (var i = 0; i < features.length; i++) {
-  var opt = features[i];
+var dropdowns2 = document
+  .getElementById("section-2")
+  .querySelectorAll(".dd-features");
+fillDropdown(dropdowns2, features2);
 
-  dropdowns.forEach((dd, j) => {
-    var el = document.createElement("option");
-    if (i < 5 && i === j) el.selected = true;
-    el.textContent = opt;
-    el.value = opt;
-    dd.append(el);
-  });
+var dropdowns3 = document
+  .getElementById("section-3")
+  .querySelectorAll(".dd-features");
+fillDropdown(dropdowns3, features3);
+
+/**
+ * Helper function to fill a dropdown with a list of features
+ * @param {*} element The dropdown element
+ * @param {*} features The list of features
+ */
+function fillDropdown(element, features) {
+  for (var i = 0; i < features.length; i++) {
+    var opt = features[i];
+
+    element.forEach((dd, j) => {
+      var el = document.createElement("option");
+      if (i < 5 && i === j) el.selected = true;
+      el.textContent = opt;
+      el.value = opt;
+      dd.append(el);
+    });
+  }
 }
 
 /*---------- PLOTTING ----------*/
 // Drawing an empty canvas
-let id = "#spider-chart";
-let width = 800;
-let height = 800;
+function drawChart(id, title, selectedFeatures, selectedValues) {
+  let width = 800;
+  let height = 800;
 
-function drawChart() {
   // Remove previous chart
   d3.select(id).select("svg").remove();
 
@@ -67,8 +82,17 @@ function drawChart() {
   let svg = d3
     .select(id)
     .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("id", id)
+    .attr("viewBox", `0 0 ${width} ${height}`);
+
+  // Add title
+  svg
+    .append("text")
+    .attr("x", width / 2)
+    .attr("y", 40)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "32px")
+    .text(title);
 
   // Drawing the grid lines
   let radialScale = d3.scaleLinear().domain([0, 10]).range([0, 250]);
@@ -113,7 +137,7 @@ function drawChart() {
       name: f,
       angle: angle,
       line_coord: angleToCoordinate(angle, 10),
-      label_coord: angleToCoordinate(angle, 12),
+      label_coord: angleToCoordinate(angle, 11),
     };
   });
 
@@ -132,6 +156,29 @@ function drawChart() {
         .attr("stroke-width", 1.5)
     );
 
+  // Draw a clip path for the labels to follow
+  svg
+    .append("defs")
+    .append("clipPath")
+    .attr("id", "label-clip-path")
+    .append("circle")
+    .attr("cx", width / 2)
+    .attr("cy", height / 2)
+    .attr("r", radialScale(11));
+
+  /**
+   * Helper function to calculate the rotation angle based on the label's position
+   * @param {*} coordinates The coordinates of the label
+   * @returns The label's rotation
+   */
+  function calculateLabelRotationAngle(coordinates) {
+    const angle = Math.atan2(
+      coordinates.y - height / 2,
+      coordinates.x - width / 2
+    );
+    return (angle * 180) / Math.PI + 90;
+  }
+
   // Draw the label
   svg
     .selectAll("label")
@@ -142,6 +189,13 @@ function drawChart() {
         .attr("x", (d) => d.label_coord.x)
         .attr("y", (d) => d.label_coord.y)
         .attr("text-anchor", "middle")
+        .attr(
+          "transform",
+          (d) =>
+            `rotate(${calculateLabelRotationAngle(d.label_coord)}, ${
+              d.label_coord.x
+            }, ${d.label_coord.y})`
+        )
         .text((d) => d.name)
     );
 
@@ -165,7 +219,7 @@ function drawChart() {
     return coordinates;
   }
 
-  // Draw the polygone
+  // Draw the polygon
   svg
     .selectAll("path")
     .data([selectedValues])
@@ -180,12 +234,61 @@ function drawChart() {
         .attr("stroke-opacity", 1)
         .attr("opacity", 0.5)
     );
+
+  // Draw little circles
+  svg
+    .selectAll("circle.intersect")
+    .data(getPathCoordinates(selectedValues))
+    .join((enter) =>
+      enter
+        .append("circle")
+        .attr("class", "intersect")
+        .attr("cx", (d) => d.x)
+        .attr("cy", (d) => d.y)
+        .attr("r", 5)
+        .attr("fill", "hsl(214, 89%, 52%)")
+    );
 }
 
-drawChart();
+// Initialize all charts
+const id1 = "#spider-chart";
+const title1 = "Fachkompetenzen";
+const sF1 = [
+  features1[0],
+  features1[1],
+  features1[2],
+  features1[3],
+  features1[4],
+];
+const sV1 = [10, 10, 10, 10, 10];
+drawChart(id1, title1, sF1, sV1);
+
+const id2 = "#spider-chart-2";
+const title2 = "Selbstkompetenzen";
+const sF2 = [
+  features2[0],
+  features2[1],
+  features2[2],
+  features2[3],
+  features2[4],
+];
+const sV2 = [10, 10, 10, 10, 10];
+drawChart(id2, title2, sF2, sV2);
+
+const id3 = "#spider-chart-3";
+const title3 = "Sozialkompetenzen";
+const sF3 = [
+  features3[0],
+  features3[1],
+  features3[2],
+  features3[3],
+  features3[4],
+];
+const sV3 = [10, 10, 10, 10, 10];
+drawChart(id3, title3, sF3, sV3);
 
 /**
- * Update the chart
+ * Update the first chart
  */
 document.getElementById("chart-form").addEventListener("submit", (e) => {
   e.preventDefault();
@@ -205,5 +308,82 @@ document.getElementById("chart-form").addEventListener("submit", (e) => {
     }
   }
 
-  drawChart();
+  drawChart(id1, title1, selectedFeatures, selectedValues);
+  downloadChart(id1, "Fachkompetenzen.svg");
 });
+
+/**
+ * Update the second chart
+ */
+document.getElementById("chart-form-2").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const data = new FormData(e.target);
+
+  // Reset all data
+  selectedFeatures = [];
+  selectedValues = [];
+
+  // Add new data
+  for (i = 1; i <= 6; i++) {
+    const feature = data.get(`dd-features-${i}`);
+    if (feature != -1) {
+      selectedFeatures.push(feature);
+      selectedValues.push(data.get(`in-features-${i}`) / 10);
+    }
+  }
+
+  drawChart(id2, title2, selectedFeatures, selectedValues);
+  downloadChart(id2, "Selbstkompetenzen.svg");
+});
+
+/**
+ * Update the third chart
+ */
+document.getElementById("chart-form-3").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const data = new FormData(e.target);
+
+  // Reset all data
+  selectedFeatures = [];
+  selectedValues = [];
+
+  // Add new data
+  for (i = 1; i <= 5; i++) {
+    const feature = data.get(`dd-features-${i}`);
+    if (feature != -1) {
+      selectedFeatures.push(feature);
+      selectedValues.push(data.get(`in-features-${i}`) / 10);
+    }
+  }
+
+  drawChart(id3, title3, selectedFeatures, selectedValues);
+  downloadChart(id2, "Sozialkompetenzen.svg");
+});
+
+/**
+ * Helper function to download any svg chart
+ * @param {*} id The id of the element
+ */
+function downloadChart(id, fileName) {
+  // Get svg element
+  const svg = document.getElementById(id);
+
+  // Create a new blob containing the svg data
+  const blob = new Blob([new XMLSerializer().serializeToString(svg)], {
+    type: "image/svg+xml",
+  });
+
+  // Create a data URL for the svg blob
+  const url = URL.createObjectURL(blob);
+
+  // Set the link
+  const btn = document.getElementById(`btn-${id}`);
+  btn.href = url;
+  btn.download = fileName;
+}
+
+downloadChart(id1, `${title1}.svg`);
+downloadChart(id2, `${title2}.svg`);
+downloadChart(id3, `${title3}.svg`);
